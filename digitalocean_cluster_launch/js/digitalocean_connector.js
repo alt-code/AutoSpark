4677b2376e7bcb2663c0252288c79934886a04de
Saurabhh <sjoshi6@ncsu.edu>
2015-09-11 12:30:12 -0400

    Added ssh key check for digital ocean setup; using nodejs promise

--- digitalocean_cluster_launch/js/digitalocean_connector.js
+++ digitalocean_cluster_launch/js/digitalocean_connector.js
@@ -2,6 +2,7 @@
 var DIGITALOCEAN = require('dropletapi').Droplets;
 var SETTINGS = require('./settings.js')
 var request = require('request');
+var promise = require('promise')
 
 // Creating API tokens
 var digitalocean = new DIGITALOCEAN(SETTINGS.token);
@@ -18,14 +19,84 @@ var private_networking = null
 var count = 3
 
 var ssh_json = {
+
     "name": SETTINGS.key_name,
     "public_key": SETTINGS.ssh_public_key
 }
 
 // Create Public SSH KEYS in digital ocean
-function create_ssh_keys(json_data) {
 
-    console.log(json_data)
+function check_ssh_key(ssh_json) {
+
+    return new promise(function(resolve, reject) {
+
+        var ssh_key_arr = []
+
+        request({
+            url: "https://api.digitalocean.com/v2/account/keys",
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+                "Authorization": "Bearer " + SETTINGS.token
+            },
+        }, function(error, response, body) {
+            if (!error || response.status_code == 200) {
+                body_json = JSON.parse(body)
+                ssh_key_arr = body_json["ssh_keys"]
+                var bool_found_key = check_for_key(ssh_json, ssh_key_arr)
+                console.log(body_json["ssh_keys"]);
+                resolve(bool_found_key)
+
+            }
+        })
+    })
+}
+
+function check_for_key(ssh_json, ssh_key_arr) {
+    check_for_name = ssh_json["name"]
+    check_public_key = ssh_json["public_key"]
+    found_key = false
+    error = false
+
+    for (var i = 0; i < ssh_key_arr.length; i++) {
+        ssh_key_blob = ssh_key_arr[i]
+        if (ssh_key_blob["name"] === check_for_name) {
+            if (ssh_key_blob["public_key"] === check_public_key) {
+                found_key = true
+                error = false
+            } else {
+                console.log("Error: Key with the name " + check_for_name + " exists ; But the public_key doesnot match")
+                console.log("Program Exit; Delete the old key first")
+                found_key = false
+                error = true
+            }
+        }
+    }
+
+    if (found_key === false && error === false) {
+        for (var i = 0; i < ssh_key_arr.length; i++) {
+            ssh_key_blob = ssh_key_arr[i]
+            if (ssh_key_blob["public_key"] === check_public_key) {
+                console.log("Error: Same public_key found in " + ssh_key_blob["name"] + " key_name")
+                found_key = false
+                error = true
+            }
+        }
+    }
+
+    if (found_key === true && error === false) {
+        return true
+    } else if (error) {
+        return null
+    } else {
+        return false
+    }
+
+}
+
+
+
+function create_ssh_keys(json_data) {
 
     request({
         url: "https://api.digitalocean.com/v2/account/keys",
@@ -38,7 +109,7 @@ function create_ssh_keys(json_data) {
         json: json_data
     }, function(error, response, body) {
         if (!error || response.status_code == 200) {
-            console.log(body);
+
         }
     })
 }
@@ -88,7 +159,14 @@ function create_dictionary(node_name) {
     return newDropletData
 }
 // Inserting SSH keys
-//create_ssh_keys(ssh_json)
+var key_promise = check_ssh_key(ssh_json)
+key_promise.then(function(ssh_key_present) {
+    console.log(ssh_key_present)
+    if (ssh_key_present === false) {
+        create_ssh_keys(ssh_json)
+    }
+
+})
 
 // Launching droplets cluster
-launch_cluster(count, cluster_name)
+//launch_cluster(count, cluster_name)
