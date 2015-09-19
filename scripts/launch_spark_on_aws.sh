c9c511ab15e89d60aa778167fae44e0ce1c019ea
Saurabhh <sjoshi6@ncsu.edu>
2015-09-19 15:31:42 -0400

    v1 of launch scripts

---
+++ scripts/launch_spark_on_aws.sh
@@ -0,0 +1,20 @@
+
+current_dir="$(pwd)"
+
+# Moving to python dir
+cd "$current_dir/../aws_cluster_launch/python"
+
+# Executing python command
+python ec2_connector.py --name spark001 --count 3 --type t2.micro --region us-west-2 --key_name ansible_key --security_group spark_cluster
+
+# Moving to ansible directory
+cd "$current_dir/../Ansible/playbooks"
+
+# Executing master script
+./master.sh
+
+# Sleep for master to get setup
+sleep 60
+
+# Executing slave script
+./slave.sh
