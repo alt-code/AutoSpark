ac4cff99850cf3dcb9bf7a720ee45bf1c2cb1ce7
Saurabhh <sjoshi6@ncsu.edu>
2015-09-23 23:28:25 -0400

    spark job

---
+++ Spark_Jobs/tweet_scanner.py
@@ -0,0 +1,56 @@
+import os.path
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+
+
+def main():
+    # Setting the cluster configuration parameters
+    conf = SparkConf()
+    conf.setMaster("spark://localhost:7077")
+    conf.setAppName("Tweet App")
+    conf.set("spark.executor.memory", "7g")
+
+    # Creating a Spark Context with conf file
+    sc = SparkContext(conf=conf)
+
+    # Creating and SQL context to perform SQL queries
+    sqlContext = SQLContext(sc)
+
+    # Define the data path
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    json_name = "tweets.json"
+
+    json_file_path = os.path.join(curr_path +
+                                  "/../Spark_Jobs/data/",
+                                  json_name)
+    createSQLContext(json_file_path, sqlContext)
+    word_count(sc, json_file_path, curr_path)
+
+
+def createSQLContext(json_file_path, sqlContext):
+
+    # Read the data into a data frame
+    dataframe = sqlContext.read.json(json_file_path)
+
+    # Print the JSON Schema
+    dataframe.printSchema()
+
+
+# Tokenizes text
+def tokenizer(data):
+    return data.split()
+
+
+# Word counter
+def word_count(sc, json_file_path, curr_path):
+    data = sc.textFile(json_file_path)
+    wc_data = data.flatMap(tokenizer)
+    wc = wc_data.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+    output_file_path = os.path.join(curr_path +
+                                    "/../Spark_Jobs/data/",
+                                    "out")
+    wc.saveAsTextFile(output_file_path)
+
+
+if __name__ == "__main__":
+    main()
