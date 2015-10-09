e39547ebf9021b839dd38854b3868df49f2e2d23
sjoshi6 <sjoshi6@ncsu.edu>
2015-10-09 17:22:36 -0400

    Interactive launch for DO

--- scripts/main_launcher.sh
+++ scripts/main_launcher.sh
@@ -19,23 +19,36 @@ then
     echo -n "Choose one cluster size or use custom > "
     read cluster_size
 
+    echo -n "Enter cluster name > "
+    read clustername
+
+    echo -n "Enter complete path to ssh private key > "
+    read key_path
     echo ""
 
     if [ "$cluster_size" = "small" ]
     then
         echo "Info: Small Cluster Size selected"
+        ./launch_spark_on_do.sh $clustername 4 512mb $key_path
 
     elif [ "$cluster_size" = "medium" ]
     then
         echo "Info: Medium Cluster Size selected"
+        ./launch_spark_on_do.sh $clustername 4 1gb $key_path
 
     elif [ "$cluster_size" = "large" ]
     then
         echo "Info: Large Cluster Size selected"
+        ./launch_spark_on_do.sh $clustername 4 2gb $key_path
 
     elif [ "$cluster_size" = "custom" ]
     then
         echo "Info: Custom Selected"
+        echo -n "Enter Number of Nodes > "
+        read count
+        echo -n "Enter digital ocean size ( 1gb/ 2gb ) > "
+        read size
+        ./launch_spark_on_do.sh $clustername $count $size $key_path
     else
         echo "Error: incorrect choice"
     fi
@@ -76,7 +89,7 @@ then
         echo "Info: Custom Selected"
         echo -n "Enter Number of Nodes > "
         read count
-        echo -n "Enter the aws instance type > "
+        echo -n "Enter the aws instance type (t2.micro / t2.large) > "
         read size
         ./launch_spark_on_aws.sh $clustername $count $size $key_path
     else
