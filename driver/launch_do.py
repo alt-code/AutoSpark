97d435d4ab87581872a2aaebd5b64311c06f6883
sjoshi6 <sjoshi6@ncsu.edu>
2015-10-25 19:59:23 -0400

    Added python driver for digitalocean launcher

--- driver/launch_do.py
+++ driver/launch_do.py
@@ -5,50 +5,42 @@ import sys
 import time
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-AWS_LAUNCHER_DIR = BASE_DIR + "/../connector/aws/"
+DO_LAUNCH_DIR = BASE_DIR + "/../connector/digital_ocean/"
 ANSIBLE_DIR = BASE_DIR + "/../Ansible/playbooks/"
 
 
-def install_packages(filename):
-
-    with open(filename) as req_file:
-        for package in req_file.readlines():
-            if package != "\n":
-                subprocess.call(['pip', 'install', package])
-
-
 def launch(args):
-    # Moving to the AWS Launcher dir
-    os.chdir(AWS_LAUNCHER_DIR)
+    # Moving to the Digital Ocean Launcher dir
+    os.chdir(DO_LAUNCH_DIR)
 
     # Printing the current dir
     print(os.getcwd())
 
     # Installing all packages
-    install_packages("requirements.txt")
+    subprocess.call("npm install", shell=True)
 
     # Getting the IP Address of machine
     ip_addr = socket.gethostbyname(socket.gethostname())
     print("IP Address: " + ip_addr)
 
     # Setting key name to ipaddress & location
-
     key_name = ip_addr
-    region = "us-west-2"
+    region = "nyc3"
     name = args[0]
     count = args[1]
-    type = args[2]
+    size = args[2]
     key_path = args[3]
 
     # Running python command
-    cmd_format = "python ec2_connector.py --name {0} --count {1} --type {2} --key_path {3} --region {4} --key_name {5}"
-    command = cmd_format.format(name, count, type, key_path, region, key_name)
+    cmd_format = "sudo node digitalocean_connector.js {0} {1} {2} {3} {4} {5}"
+    command = cmd_format.format(name, count, size, region, key_name, key_path)
     print("Executing Command" + command)
 
     subprocess.call(command, shell=True)
+    subprocess.call("sudo node create_shell_scripts.js", shell=True)
 
     # Wait for instance to be ssh ready
-    print("Waiting for ec2 instances to be ready for ssh")
+    print("Waiting for digital ocean instances to be ready for ssh")
     time.sleep(100)
 
     # Move to ansible directory
