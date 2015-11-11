c6ede29cce047a6196c3c7712489c5b050d21ae6
sjoshi6 <sjoshi6@ncsu.edu>
2015-11-11 13:12:35 -0500

    Modified submit for digitalocean

--- driver/autospark-submit.js
+++ driver/autospark-submit.js
@@ -20,8 +20,9 @@ function command_executor(cmd) {
 	});
 }
 
-prompt.get(['spark_master_ip','spark_context_url','spark_job_file_path', 'job_name_at_destination', 'data_file_name'], function (err, result) {
+prompt.get(['provider','spark_master_ip','spark_context_url','spark_job_file_path', 'job_name_at_destination', 'data_file_name'], function (err, result) {
 
+    provider = result.provider
     spark_master_ip = result.spark_master_ip
     spark_context_url = result.spark_context_url
     spark_job_file_path = result.spark_job_file_path
@@ -29,13 +30,27 @@ prompt.get(['spark_master_ip','spark_context_url','spark_job_file_path', 'job_na
     data_file_name = result.data_file_name
 
     // Executing the spark job
-    console.log("Copying the program to the remote spark master...")
-    cmd = "scp " + spark_job_file_path + " " + spark_master_ip + ":/home/ubuntu/" + job_name_at_destination
-    command_executor(cmd)
+    if (provider === 'aws') {
 
-    console.log("Running spark job on master...")
-    cmd = "ssh -l ubuntu " + spark_master_ip + " 'sudo /spark/spark_latest/bin/pyspark /home/ubuntu/"+ job_name_at_destination + " " + spark_context_url + " " + data_file_name + "'"
-    command_executor(cmd)
+        console.log("Copying the program to the remote spark master...")
+        cmd = "scp -i " + ssh_private_key_path + " " + spark_job_file_path + " " + spark_master_ip + ":/home/ubuntu/" + job_name_at_destination
+        command_executor(cmd)
+
+        console.log("Running spark job on master...")
+        cmd = "ssh -l ubuntu " + spark_master_ip + " 'sudo /spark/spark_latest/bin/pyspark /home/ubuntu/"+ job_name_at_destination + " " + spark_context_url + " " + data_file_name + "'"
+        command_executor(cmd)
+
+    } else if (provider === 'digitalocean') {
+
+        console.log("Copying the program to the remote spark master...")
+        cmd = "scp -i " + ssh_private_key_path + " " + spark_job_file_path + " root@" + spark_master_ip + ":/home/ubuntu/" + job_name_at_destination
+        command_executor(cmd)
+
+        console.log("Running spark job on master...")
+        cmd = "ssh -l root " + spark_master_ip + " 'sudo /spark/spark_latest/bin/pyspark /home/ubuntu/"+ job_name_at_destination + " " + spark_context_url + " " + data_file_name + "'"
+        command_executor(cmd)
+    }
+
 
     // Prompt ends here
     });
